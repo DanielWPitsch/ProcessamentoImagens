696867b45e0273322bb542342022931056a96281
DanielWPitsch <102161140+DanielWPitsch@users.noreply.github.com>
2024-03-13 21:09:19 -0300

    up

---
+++ Main.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedImage imagem = ManipulaImg.abrirImagem("arara.jpeg");
+        ManipulaImg.exibirImagem(imagem);
+
+        int altura = imagem.getHeight();
+        int largura = imagem.getWidth();
+
+        for(int h=0; h<altura; h++){
+            for(int w=0; w<largura; w++){
+                int rgb = imagem.getRGB(w, h);
+                Color cor = new Color(rgb);
+                int red = cor.getRed();
+                int green = cor.getGreen();
+                int blue = cor.getBlue();
+
+                System.out.println(red+" "+green+ " "+blue);
+                Color novaCor = new Color(0, 0, blue);
+                imagem.setRGB(w, h, novaCor.getRGB());
+            }
+        }
+    }
+}
